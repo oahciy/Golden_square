fd681fa88eec1d0214889db96788a5057526133d
Yichao <yq2550@gmail.com>
2022-08-16 17:05:19 +0100

    all test passed, start refactoring

--- lib/reading_time.rb
+++ lib/reading_time.rb
@@ -1,3 +1,13 @@
 def reading_time(text)
-  return "There is nothing to read."
+  words_count = text.split.length
+  reading_time_sec = 0.3 * words_count
+  if text == ""
+    return "There is nothing to read."
+  elsif words_count >= 200
+    reading_time_min = (reading_time_sec / 60).to_i
+    reading_time_sec = (reading_time_sec % 60).floor(1)
+    return "Your reading time is #{reading_time_min} minute(s) #{reading_time_sec} second(s)"
+  elsif words_count < 200
+    return "Your reading time is 0 minute(s) #{reading_time_sec} second(s)"
+  end
 end
--- spec/reading_time_spec.rb
+++ spec/reading_time_spec.rb
@@ -8,4 +8,55 @@ describe "reading_time method" do
   it "returns 'Your reading time is 0 minute(s) 0.3 second(s)' when passed 'one'" do
    expect(reading_time("one")).to eq "Your reading time is 0 minute(s) 0.3 second(s)"
   end
+
+  it "returns 'Your reading time is 0 minute(s) 0.6 second(s)' when passed 'one two'" do
+    expect(reading_time("one two")).to eq "Your reading time is 0 minute(s) 0.6 second(s)"
+   end
+
+   it "returns 'Your reading time is 0 minute(s) 4.8 second(s)' when passed 'a longer text'" do
+    expect(reading_time("Once upon there was a prince who was very unhappy. He had not slept in days.")).to eq "Your reading time is 0 minute(s) 4.8 second(s)"
+   end
+
+   it "return your reading time in minutes and seconds when passed a text more than 200 words" do
+    expect(reading_time("1. Strophe
+      Hänsel und Gretel verliefen sich im Wald.
+      Es war so finster und auch so bitter kalt.
+      Sie kamen an ein Häuschen von Pfefferkuchen fein.
+      Wer mag der Herr wohl von diesem Häuschen sein?
+      
+      2. Strophe 
+      Hu, hu, da schaut eine alte Hexe raus!
+      Lockte die Kinder ins Pfefferkuchenhaus.
+      Sie stellte sich gar freundlich, o Hänsel, welche Not!
+      Ihn wollt’ sie braten im Ofen braun wie Brot.
+      
+      3. Strophe
+      Doch als die Hexe zum Ofen schaut hinein,
+      ward sie gestoßen von unserm Gretelein.
+      Die Hexe musste braten, die Kinder geh’n nach Haus.
+      Nun ist das Märchen von Hans und Gretel aus.
+      
+      1. Strophe
+      Hänsel und Gretel verliefen sich im Wald.
+      Es war so finster und auch so bitter kalt.
+      Sie kamen an ein Häuschen von Pfefferkuchen fein.
+      Wer mag der Herr wohl von diesem Häuschen sein?
+      
+      2. Strophe
+      Hu, hu, da schaut eine alte Hexe raus!
+      Lockte die Kinder ins Pfefferkuchenhaus.
+      Sie stellte sich gar freundlich, o Hänsel, welche Not!
+      Ihn wollt’ sie braten im Ofen braun wie Brot.
+      
+      Doch als die Hexe zum Ofen schaut hinein,
+      ward sie gestoßen von unserm Gretelein.
+      Die Hexe musste braten, die Kinder geh’n nach Haus.
+      Nun ist das Märchen von Hans und Gretel aus.
+      
+      Doch als die Hexe zum Ofen schaut hinein,
+      ward sie gestoßen von unserm Gretelein.
+      Die Hexe musste braten, die Kinder geh’n nach Haus.
+      Nun ist das Märchen von Hans und Gretel aus.
+      ")).to eq "Your reading time is 1 minute(s) 10.2 second(s)"
+   end
 end
