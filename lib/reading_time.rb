ca97fddce65dadbfe4b587d9ae5a004dcdc45565
Jonas Diete <jonas.diete@gmail.com>
2022-08-16 17:27:37 +0100

    refactored

--- lib/reading_time.rb
+++ lib/reading_time.rb
@@ -1,13 +1,10 @@
 def reading_time(text)
-  words_count = text.split.length
-  reading_time_sec = 0.3 * words_count
+  reading_time_sec = 0.3 * text.split.length
   if text == ""
     return "There is nothing to read."
-  elsif words_count >= 200
+  else
     reading_time_min = (reading_time_sec / 60).to_i
     reading_time_sec = (reading_time_sec % 60).floor(1)
     return "Your reading time is #{reading_time_min} minute(s) #{reading_time_sec} second(s)"
-  elsif words_count < 200
-    return "Your reading time is 0 minute(s) #{reading_time_sec} second(s)"
   end
 end
