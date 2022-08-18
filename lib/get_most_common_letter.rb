1643d90723153f13af6b2831712ac9af87788d8a
Liovirgilda Mendonca <liovirgildamendonca@liovirgildasair.home>
2022-08-18 14:27:54 +0100

    debugging 04 challenge finished

--- lib/get_most_common_letter.rb
+++ lib/get_most_common_letter.rb
@@ -1,13 +1,14 @@
 def get_most_common_letter(text)
   counter = Hash.new(0)
-  p counter
-  text.chars.each do |char|
+  text.gsub!(/[!@#$%^&*()-=_+|;':",.<>?' ]/, '')
+  text.downcase.chars.each do |char|
     counter[char] += 1
   end
-  counter.to_a.sort_by { |k, v| v }[0][0]
+  counter.to_a.sort_by { |k, v| v }[-1][0]
 end
 
 p get_most_common_letter("the roof, the roof, the roof is on fire!")
+p get_most_common_letter("get groceries!")
 # Intended output:
 # 
 # > get_most_common_letter("the roof, the roof, the roof is on fire!")
