a36453e25dc1557dff26b7ee6a8e679db3ead1ac
Yichao <yq2550@gmail.com>
2022-08-30 11:08:49 +0100

    todo unit test done

--- lib/08_todo.rb
+++ lib/08_todo.rb
@@ -1,6 +1,7 @@
 class Todo
   def initialize(task) # task is a string
     @task = task
+    @done = false
   end
 
   def task
@@ -8,12 +9,10 @@ class Todo
   end
 
   def mark_done!
-    # Marks the todo as done
-    # Returns nothing
+    @done = true
   end
 
   def done?
-    # Returns true if the task is done
-    # Otherwise, false
+    @done
   end
 end
--- spec/08_todo_spec.rb
+++ spec/08_todo_spec.rb
@@ -3,6 +3,29 @@ require "08_todo"
 RSpec.describe Todo do
   it "initialize with a task" do
     todo = Todo.new("walk the dog")
-    expect(todo.taks).to eq "walk the dog"
+    expect(todo.task).to eq "walk the dog"
+  end
+
+  context "only one todo" do
+    it "marks done and returns true" do
+      todo = Todo.new("walk the dog")
+      todo.mark_done!
+      expect(todo.done?).to eq true
+    end
+
+    it "returns false" do
+      todo = Todo.new("walk the dog")
+      expect(todo.done?).to eq false
+    end
+  end
+
+  context "multiple todos" do
+    it "returns status" do
+      todo1 = Todo.new("walk the dog")
+      todo2 = Todo.new("Parking Permit")
+      todo1.mark_done!
+      expect(todo1.done?).to eq true
+      expect(todo2.done?).to eq false 
+    end
   end
 end
